c9eebbdfbb2cd2556100e7fbddf2fb52103fba41
Ex-Dev Robin <exdev.robin@outlook.com>
2020-07-10 22:05:00 +0530

    v1.00.02dev
    Dev : exdev.robin
    Date: 10th July 2020
    Desc: Rest API Added for UserController.

--- src/main/java/com/example/demo/SnsMiddlewareApplication.java
+++ src/main/java/com/example/demo/SnsMiddlewareApplication.java
@@ -2,14 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude=HibernateJpaAutoConfiguration.class)
 @ComponentScan(basePackages = "org.hov")
 public class SnsMiddlewareApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SnsMiddlewareApplication.class, args);
 	}
-
 }
--- src/main/java/org/hov/controller/UserController.java
+++ src/main/java/org/hov/controller/UserController.java
@@ -2,12 +2,13 @@ package org.hov.controller;
 
 import java.util.UUID;
 
+import javax.validation.Valid;
+
 import org.hov.model.User;
 import org.hov.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,28 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
-@CrossOrigin(value = "")
+//@CrossOrigin(value = "")
 public class UserController {
 	@Autowired
 	UserService userService;
-	
-	@GetMapping("/byemail/{email}")
-	public ResponseEntity<User> getUserByEmail(@PathVariable("email") String email){
-		return new ResponseEntity<User>(userService.getUserByEmail(email), HttpStatus.OK);
-	}
-	
-	@GetMapping("/byphone/{phone}")
-	public ResponseEntity<User> getUserByPhone(@PathVariable("phone") String phone){
-		return new ResponseEntity<User>(userService.getUserByPhone(phone), HttpStatus.OK);
-	}
-	
-	@GetMapping("/byid/{id}")
-	public ResponseEntity<User> getUserById(@PathVariable("id") String id){
-		return new ResponseEntity<User>(userService.getUserById(UUID.fromString(id)), HttpStatus.OK);
-	}
 
 	@PostMapping("/add")
-	public ResponseEntity<Void> addUser(@RequestBody User user){
+	public ResponseEntity<Void> addUser(@Valid @RequestBody User user){
 		if(userService.addUser(user)){
 			return new ResponseEntity<Void>(HttpStatus.CREATED);
 		}
@@ -50,7 +36,7 @@ public class UserController {
 	}
 
 	@PutMapping("/update")
-	public ResponseEntity<Void> updateUser(@RequestBody User user){
+	public ResponseEntity<Void> updateUser(@Valid @RequestBody User user){
 		if(userService.updateUser(user)){
 			return new ResponseEntity<Void>(HttpStatus.OK);
 		}
@@ -67,5 +53,49 @@ public class UserController {
 		else {
 			return new ResponseEntity<Void>(HttpStatus.UNPROCESSABLE_ENTITY);
 		}
+	}
+	
+	@GetMapping("/byemail/{email}")
+	public ResponseEntity<User> getUserByEmail(@PathVariable("email") String email){
+		return new ResponseEntity<User>(userService.getUserByEmail(email), HttpStatus.OK);
+	}
+	
+	@GetMapping("/byphone/{phone}")
+	public ResponseEntity<User> getUserByPhone(@PathVariable("phone") String phone){
+		return new ResponseEntity<User>(userService.getUserByPhone(phone), HttpStatus.OK);
+	}
+	
+	// BELOW CODES ARE UNDER DEVELOPMENT
+	@GetMapping("/profile")
+	public ResponseEntity<User> getUserById(@PathVariable("id") String id){
+		return new ResponseEntity<User>(userService.getUserById(UUID.fromString(id)), HttpStatus.OK);
+	}
+	
+	@GetMapping("/wishlist")
+	public ResponseEntity<User> getWishListById(@PathVariable("id") String id){
+		return new ResponseEntity<User>(userService.getUserById(UUID.fromString(id)), HttpStatus.OK);
+	}
+	
+	@GetMapping("/cartlist")
+	public ResponseEntity<User> getCartListById(@PathVariable("id") String id){
+		return new ResponseEntity<User>(userService.getUserById(UUID.fromString(id)), HttpStatus.OK);
+	}
+	
+	@GetMapping("/orderlist")
+	public ResponseEntity<User> getOrderListById(@PathVariable("id") String id){
+		return new ResponseEntity<User>(userService.getUserById(UUID.fromString(id)), HttpStatus.OK);
+	}
+	
+	/* Login */	
+	@PostMapping("/login")
+	public ResponseEntity<User> userLogin(@RequestBody User user){
+		return new ResponseEntity<User>(userService.getUserById(UUID.fromString("")), HttpStatus.OK);
+		//return new ResponseEntity<User>(new FieldError("user", "email", "already exists"), HttpStatus.BAD_REQUEST);;
+	}
+	
+	/* Logout */	
+	@PostMapping("/logout")
+	public ResponseEntity<User> userLogoff(@RequestBody User user){
+		return new ResponseEntity<User>(userService.getUserById(UUID.fromString("")), HttpStatus.OK);
 	}
 }
